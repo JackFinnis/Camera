145049c0ea28551c80b0b611d08076662c72a977
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-09-16 15:27:43 +0100

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Jack Finnis on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
